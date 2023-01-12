d7c230b4bd32adcda5b379311e5f3fc11fe79a81
Alan Wong <95393427+alantmwong@users.noreply.github.com>
2023-01-12 03:09:28 +0000

    Initial commit

--- main.py
+++ main.py
@@ -31,90 +31,74 @@ import random
 #Hint 3: Download and read this flow chart I've created:
 #   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+# Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
+def deal_card():
+  """Returns a random card from the deck"""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input
-#and returns the score.
-#Look up the sum() function to help you do this.
+# Create a function called calculate_score() that takes a List of cards as input
+# and returns the score. 
+  
+def calculate_score(hand):
+  """Takes a list of cards and returns the score"""
+  score = sum(hand)
+  # Check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 
+  # instead of the actual score. 0 will represent a blackjack in our game. 
+  if len(hand) == 2 and score == 21:
+    return 0
+  # Removing the 11 (Ace) inside a hand if the score is over 21 to become 
+  # a value of 1
+  if 11 in hand and score > 21:
+    hand.remove(11)
+    hand.append(1)
+    
+  return score
 
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+def compare():
+  
+  
+# Deal the user and computer 2 cards each using deal_card() and append().
+user_cards = []
+user_cards.append(deal_card())
+user_cards.append(deal_card())
+computer_cards = []
+computer_cards.append(deal_card())
+computer_cards.append(deal_card())
 
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+game_over = False
+while game_over == False:
+  # The score will need to be rechecked with every new card drawn and the checks
+  # need to be repeated until the game ends.
+  user_score = calculate_score(user_cards)
+  computer_score = calculate_score(computer_cards)
+  print(f"  Your cards: {user_cards}, current score: {user_score}")
+  print(f"  Computer's first card: {computer_cards[0]}")
+  
+  if computer_score == 0 or user_score == 0 or user_score > 21:
+    game_over = True
+  # If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+  else: 
+    hit = input("Do you want to draw another card?: Type 'y' or 'n' \n")
+    if hit == 'y':
+      user_cards.append(deal_card())
+    else: 
+      game_over = True
+  
 
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+# Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+while computer_score != 0 and computer_score < 17:
+  computer_cards.append(deal_card())
+  computer_score = calculate_score(copmuter_cards)
 
 #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 
-continue_game = input("Do you want to play blackjack? Type 'y' or 'n' \n")
+# continue_game = input("Do you want to play blackjack? Type 'y' or 'n' \n")
 
 
-def check_continue_game(continue_game):
-    if continue_game == 'y':
-        continue_game = True
-        return continue_game
-    else:
-        continue_game = False
-        return continue_game
-
-
-def deal_player_card():
-    player_cards.append(random.randint(0, 12))
-    return player_cards
-
-
-def deal_computer_card():
-    computer_cards.append(random.randint(0, 12))
-    return computer_cards
-
-
-def calculate_score(cards):
-    score = 0
-    for card in cards:
-        score += card
-    return score
-
-
-def print_player_hand(player_cards, player_score):
-    print(f"  Your cards: {player_cards}, current score: {player_score}")
-
-
-def hit_or_stand(player_cards, player_score):
-    hit = input("Type 'y' to get another card, type 'n' to pass: ")
-    if hit == 'y':
-        deal_player_card()
-        print_player_hand()
-      
-def starting_deal():
-  player_cards = deal_player_card()
-  player_cards = deal_player_card()
-  player_score = calculate_score(player_cards)
-  computer_cards = deal_computer_card()
-  print_player_hand(player_cards, player_score)
-  
-continue_game = check_continue_game(continue_game)
-
-from art import logo
-
-print(logo)
-
-while continue_game == True:
-    player_cards = []
-    computer_cards = []
-    starting_deal()
-    print(f"  Computer's first card: {computer_cards[0]}")
-      
-    continue_game = False
